6f6ba72cb6535205f05d8baae14cb659a19a5aef
Antriksh Sharma <archieskts1@gmail.com>
2022-09-01 23:02:22 +0530

    pehla commit

---
+++ RestaurantManagementSystem/src/main/java/com/mycompany/restaurantmanagementsystem/RestaurantManagementSystem.java
@@ -0,0 +1,8 @@
+package com.mycompany.restaurantmanagementsystem;
+
+public class RestaurantManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
