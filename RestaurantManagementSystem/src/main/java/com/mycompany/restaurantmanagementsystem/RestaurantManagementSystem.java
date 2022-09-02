d3fd526a0867ba2f37a81e00cd9f626ba55af5df
Antriksh Sharma <archieskts1@gmail.com>
2022-09-02 17:29:47 +0530

    safai

--- RestaurantManagementSystem/src/main/java/com/mycompany/restaurantmanagementsystem/RestaurantManagementSystem.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.restaurantmanagementsystem;
-
-public class RestaurantManagementSystem {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
